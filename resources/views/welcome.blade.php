8c4b0dc626a0783e69183ea9ac0875a1f141a91e
mattp <matt.permentier@gmail.com>
2022-09-16 13:51:18 +0200

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,7 @@
                         </g>
                     </svg>
                 </div>
-                <h1 class="mt-2 text-gray-600 dark:text-gray-400 text-l">Hello World</h1>
+                <h1 class="mt-2 text-gray-600 dark:text-gray-400 text-l">Test</h1>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
