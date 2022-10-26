5a0277a757dd25bac863cac6eed66a7489b35855
mattp <matt.permentier@gmail.com>
2022-10-26 20:37:41 +0200

    comment added

--- app/Http/Controllers/YourBikeController.php
+++ app/Http/Controllers/YourBikeController.php
@@ -11,6 +11,7 @@ class YourBikeController extends Controller
 
     public function yourBikes()
     {
+//        get bikes where user_id is equal to logged in user
         $yourBikes = Bike::where("user_id", Auth::user()->id)->get();
 
         return view('yourBikes',
